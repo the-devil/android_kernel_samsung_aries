f3c6e7a3aab6aca4988ac2d8c811e5b44d7bd1b9
Dominik Baronelli <dominik.baronelli@gmail.com>
2012-02-05 08:40:00 +0100

    disabled zcache defaul

--- drivers/staging/zcache/zcache-main.c
+++ drivers/staging/zcache/zcache-main.c
@@ -1888,7 +1888,7 @@ struct frontswap_ops zcache_frontswap_register_ops(void)
  */
 
 /* always enabled when selected */
-static int zcache_enabled = 1;
+static int zcache_enabled;
 
 static int __init enable_zcache(char *s)
 {
